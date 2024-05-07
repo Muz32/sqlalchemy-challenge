abe960a50211ea0b4d8a9dee4270d43fc8e510b0
Muz32 <muzaffar.amin@live.com>
2024-05-08 01:17:01 +1000

    edit on code comments

--- Surfsup/app.py
+++ Surfsup/app.py
@@ -12,27 +12,31 @@ from flask import Flask, jsonify
 
 # Create engine using the `hawaii.sqlite` database file
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
 # Declare a Base using `automap_base()`
 Base = automap_base()
+
 # Use the Base class to reflect the database tables
 Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
 # Create a session
 session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
+
 app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -62,7 +66,7 @@ def precipitation():
     last_date = session.query(func.max(Measurement.date)).scalar()
     query_date = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
 
-    # Perform a query to join 'measurement' and 'station' tables on their 'station' columns
+    # Perform a query to join 'measurement' and 'station' classes their 'station' attribute (column)
     query_result = session.query(Measurement.station, Measurement.date, Measurement.prcp).\
         join(Station, Measurement.station == Station.station).\
         filter(Measurement.date >= query_date).all()
